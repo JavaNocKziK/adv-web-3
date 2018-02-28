5c0db8a3d9613ab363368b879a84f6e4001451d4
JavaNocKziK <javanockzik@googlemail.com>
2018-02-28 20:19:07 +0000

    Updated User Model for Authentication
    - Changed the schema to mark the data as required.
    - Added a pre to the save so encrypt passwords when we save the user.
    - Added authenticate static to check passwords.

--- server/mongodb/models/user.model.js
+++ server/mongodb/models/user.model.js
@@ -1,13 +1,65 @@
 const mongoose = require('mongoose');
-
-let Schema = mongoose.Schema;
-let UserSchema = new Schema(
+const bcrypt = require('bcrypt');
+let UserSchema = new mongoose.Schema(
     {
-        username: String,
-        password: String,
-        security: Number
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        security: {
+            type: String,
+            required: true
+        }
     },
     { versionKey: false }
 );
-
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.pre('save', function(next) {
+    let user = this;
+    bcrypt.hash(user.password, 10, (err, hash) => {
+        if(err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
+});
+UserSchema.statics.authenticate = function authenticate(username, password) {
+    return new Promise((accept, reject) => {
+        User.findOne({ username: username })
+            .exec((err, user) => {
+                if(err) {
+                    reject({
+                        "status": 0,
+                        "message": err
+                    });
+                }
+                if(!user) {
+                    reject({
+                        "status": 0,
+                        "message": "User not found."
+                    });
+                }
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if(result === true) {
+                        accept({
+                            "status": 1,
+                            "message": user
+                        });
+                    } else {
+                        reject({
+                            "status": 0,
+                            "message": "Password mismatch."
+                        });
+                    }
+                });
+            });
+    });
+};
+let User = mongoose.model('User', UserSchema);
+module.exports = User;

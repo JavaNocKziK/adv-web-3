c07f9dbaa4d6af45591ab67834dc20f64a064352
JavaNocKziK <javanockzik@googlemail.com>
2018-03-02 03:20:35 +0000

    Wait Area
    - Added a basic wait area so that I could test routing.
    - Will eventually become the actual wait area, hense why I'm committing
    it.

---
+++ src/app/components/wait-area/wait-area.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wait-area',
+  templateUrl: './wait-area.component.html',
+  styleUrls: ['./wait-area.component.scss']
+})
+export class WaitAreaComponent implements OnInit {
+    constructor() {}
+    ngOnInit() {}
+}

95663ab1010bed858dd3da5d8140ee5c418c373b
JavaNocKziK <javanockzik@googlemail.com>
2018-03-02 03:26:01 +0000

    Added Routing File and Module Updates
    - Added routing file. Only has login and wait area the moment.
    - Basic module updates with stuff we need.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './components/login/login.component';
+import { WaitAreaComponent } from './components/wait-area/wait-area.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'wait',
+        component: WaitAreaComponent
+    },
+    // Other routes here.
+    {
+        path: '404',
+        component: LoginComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/404'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {useHash: false})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http'
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
 import { UserService } from './services/user.service';
+import { WaitAreaComponent } from './components/wait-area/wait-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WaitAreaComponent
   ],
   imports: [
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [
     UserService

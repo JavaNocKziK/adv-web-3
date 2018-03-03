670b020941e939ac61f8c729d9c3c5eb79cf3cf0
JavaNocKziK <javanockzik@googlemail.com>
2018-03-03 03:04:01 +0000

    Order and Stock Classes Update
    - Added add, remove, quantity and find functions to the order class. For
    each one you pass the ID of the stock you want to manage and it will
    automatically add or remove quantity.
    - Added ID to the stock class.
    - Added getter for ID and name.

--- src/app/classes/order.ts
+++ src/app/classes/order.ts
@@ -1,15 +1,48 @@
 export class Order {
     private _items: OrderItem[] = [];
     constructor() {}
-    public add(stock: OrderItem) {
-        this._items.push(stock);
+    public add(id: string) {
+        let index = this.find(id);
+        if(index != -1) {
+            this._items[index].quantity++;
+        } else {
+            this._items.push(new OrderItem(id, 1));
+        }
+    }
+    public remove(id: string) {
+        let index = this.find(id);
+        if(index != -1) {
+            this._items[index].quantity--;
+            if(this._items[index].quantity <= 0) {
+                this._items.splice(index, 1);
+            }
+        } else {
+            this._items.push(new OrderItem(id, 1));
+        }
+    }
+    public quantity(id: string): number {
+        let index = this.find(id);
+        if(index != -1) {
+            return this._items[index].quantity;
+        } else {
+            return 0;
+        }
     }
     public clear() {
         this._items = [];
+    }
+    private find(id: string): number {
+        return this._items.findIndex((data) => {
+            return data.stockId == id;
+        });
     }
 }
 
 export class OrderItem {
     public stockId: string;
     public quantity: number;
+    constructor(stockId: string, quantity: number) {
+        this.stockId = stockId;
+        this.quantity = quantity;
+    }
 }
--- src/app/classes/stock.ts
+++ src/app/classes/stock.ts
@@ -1,13 +1,21 @@
 export class Stock {
+    private _id: string;
     private _name: string;
     private _description: string;
     private _category: number;
     private _quantity: number;
-    constructor(name: string, description: string, category, number, quantity: number) {
+    constructor(id: string, name: string, description: string, category: number, quantity: number) {
+        this._id = id;
         this._name = name;
         this._description = description;
         this._category = category;
         this._quantity = quantity;
+    }
+    get id(): string {
+        return this._id;
+    }
+    get name(): string {
+        return this._name;
     }
     get categoryString(): string {
         switch(this._category) {

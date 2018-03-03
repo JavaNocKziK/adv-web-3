ec01b27449f81af1397bf71701b6eaec9066b4bb
JavaNocKziK <javanockzik@googlemail.com>
2018-03-03 18:30:30 +0000

    Order API Update
    - Updated the order model.

--- server/mongodb/models/order.model.js
+++ server/mongodb/models/order.model.js
@@ -3,11 +3,31 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let OrderSchema = new Schema(
     {
-        orderId: Number,
-        userId: Number,
-        tableId: Number,
-        content: [Number],
-        timeCreated: String
+        userId: {
+            type: String,
+            required: true,
+        },
+        tableId: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: [{
+                stockId: {
+                    type: String,
+                    required: true
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                }
+            }],
+            required: true
+        },
+        timeCreated: {
+            type: String,
+            required: true
+        }
     },
     { versionKey: false }
 );

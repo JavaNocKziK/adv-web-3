c445576eee5424f935b70ac881302a37722042eb
JavaNocKziK <javanockzik@googlemail.com>
2018-03-05 19:10:58 +0000

    Class Updates
    - Added new class OrderStatus that stores the order status.
    - Updated the Order class so that it has a proper constructor. Renamed _items to _content. Added a few needed getters.

--- src/app/classes/order.ts
+++ src/app/classes/order.ts
@@ -1,43 +1,66 @@
 export class Order {
-    private _items: OrderItem[] = [];
-    constructor() {}
+    private _id: string;
+    private _friendlyId: string;
+    private _content: OrderItem[] = [];
+    private _status: number;
+    private _tableId: string;
+    private _timeCreated: Date;
+    private _userId: string;
+    constructor(id?: string, friendlyId?: string, content?: OrderItem[], status?: number, tableId?: string, timeCreated?: Date, userId?: string) {
+        this._id = id;
+        this._friendlyId = friendlyId;
+        this._content = (content == undefined ? [] : content);
+        this._status = status;
+        this._tableId = tableId;
+        this._timeCreated = timeCreated;
+        this._userId = userId;
+    }
     public add(id: string) {
         let index = this.find(id);
         if(index != -1) {
-            this._items[index].quantity++;
+            this._content[index].quantity++;
         } else {
-            this._items.push(new OrderItem(id, 1));
+            this._content.push(new OrderItem(id, 1));
         }
     }
     public remove(id: string) {
         let index = this.find(id);
         if(index != -1) {
-            this._items[index].quantity--;
-            if(this._items[index].quantity <= 0) {
-                this._items.splice(index, 1);
+            this._content[index].quantity--;
+            if(this._content[index].quantity <= 0) {
+                this._content.splice(index, 1);
             }
         } else {
-            this._items.push(new OrderItem(id, 1));
+            this._content.push(new OrderItem(id, 1));
         }
     }
     public quantity(id: string): number {
         let index = this.find(id);
         if(index != -1) {
-            return this._items[index].quantity;
+            return this._content[index].quantity;
         } else {
             return 0;
         }
     }
     public clear() {
-        this._items = [];
+        this._content = [];
     }
     private find(id: string): number {
-        return this._items.findIndex((data) => {
+        return this._content.findIndex((data) => {
             return data.stockId == id;
         });
     }
     get items(): OrderItem[] {
-        return this._items;
+        return this._content;
+    }
+    get id(): string {
+        return this._id;
+    }
+    get friendlyId(): string {
+        return this._friendlyId;
+    }
+    get status(): number {
+        return this._status;
     }
 }
 

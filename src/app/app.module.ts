42731ab62c3f7949f25c76df570c80569ef196dd
Elliot <e.keen.dev@gmail.com>
2018-03-11 17:12:57 +0000

    Kitchen component created and route added at [url]/kitchen, test message displays

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { StockService } from './services/stock.service';
 import { UserService } from './services/user.service';
 import { WaitAreaComponent } from './components/wait-area/wait-area.component';
 import { OrderService } from './services/order.service';
+import { KitchenAreaComponent } from './components/kitchen-area/kitchen-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    WaitAreaComponent
+    WaitAreaComponent,
+    KitchenAreaComponent
   ],
   imports: [
     FormsModule,
---
+++ src/app/components/kitchen-area/kitchen-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kitchen-area',
+  templateUrl: './kitchen-area.component.html',
+  styleUrls: ['./kitchen-area.component.scss']
+})
+export class KitchenAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

2483755c43d29b53f3db9b5620860c76386c75ff
JavaNocKziK <javanockzik@googlemail.com>
2018-03-12 06:53:57 +0000

    Counter Area Update
    - Re-named all price variables to be underscore.
    - Tabs are now generated programatically and added in Archived tab that
    lets you view past bills (that have been paid).
    - Added in some error checking (although that might not be required as
    the order service should catch it, but it's better to have double than
    nothing).
    - Added overlay that displays errors (this will be moved to the main app
    component).

--- src/app/components/counter-area/counter-area.component.ts
+++ src/app/components/counter-area/counter-area.component.ts
@@ -3,6 +3,8 @@ import { UserService } from '../../services/user.service';
 import { Order, OrderItem } from '../../classes/order';
 import { OrderService } from '../../services/order.service';
 import { Observable } from 'rxjs/Rx';
+import { ErrorService } from '../../services/error.service';
+import { Tab } from '../../classes/tab';
 
 @Component({
     selector: 'app-counter-area',
@@ -10,63 +12,87 @@ import { Observable } from 'rxjs/Rx';
     styleUrls: ['./counter-area.component.scss']
 })
 export class CounterAreaComponent implements OnInit {
-    private orders: Order[] = [];
-    private selected: number;
+    private _errors: string[] = [];
+    private _completedOrders: Order[] = [];
+    private _selectedOrder: number;
+    private _activeTab: number = 0;
+    private _tabs: Tab[] = [];
     constructor(
+        private errorService: ErrorService,
         private orderService: OrderService,
         private userService: UserService
     ) {
-        this.fetchOrders(); // Initial fetch.
+        this._tabs.push(new Tab('Bills', 0));
+        this._tabs.push(new Tab('Archived', 1));
+        this._tabs.push(new Tab('Reports', 2));
+        this.fetchCompletedOrders(); // Initial fetch.
         Observable.interval(120000).subscribe(() => {
-            this.fetchOrders(); // Update every 2 minutes.
+            this.fetchCompletedOrders(); // Update every 2 minutes.
+        });
+        this.errorService.errors.subscribe((result) => {
+            this._errors = result;
         });
     }
     ngOnInit() {}
+    get hasError(): boolean {
+        return (this._errors.length > 0 ? true : false);
+    }
+    public clearErrors() {
+        this.errorService.clear();
+    }
     public logout() {
         this.userService.logout();
     }
     private select(index) {
-        this.selected = index;
+        this._selectedOrder = index;
     }
     private close() {
-        this.selected = undefined;
+        this._selectedOrder = undefined;
     }
-    private fetchOrders() {
+    public changeTab(index: number) {
+        this._activeTab = index;
+    }
+    private fetchCompletedOrders() {
         this.orderService.many(true, 4, undefined).subscribe((result) => {
-            let orders = result.message;
-            console.log(orders);
-            this.orders = [];
-            orders.forEach((order) => {
-                let orderItems: OrderItem[] = [];
-                order.content.forEach((item) => {
-                    orderItems.push(new OrderItem(
-                        item.stockId,
-                        item.quantity,
-                        item.stockName,
-                        item.totalPrice
+            if(result.status == 1) {
+                let orders = result.message;
+                this._completedOrders = [];
+                orders.forEach((order) => {
+                    let orderItems: OrderItem[] = [];
+                    order.content.forEach((item) => {
+                        orderItems.push(new OrderItem(
+                            item.stockId,
+                            item.quantity,
+                            item.stockName,
+                            item.totalPrice
+                        ));
+                    });
+                    this._completedOrders.push(new Order(
+                        order._id,
+                        order.friendlyId,
+                        orderItems,
+                        order.status,
+                        order.tableId,
+                        new Date(),
+                        order.userId
                     ));
                 });
-                this.orders.push(new Order(
-                    order._id,
-                    order.friendlyId,
-                    orderItems,
-                    order.status,
-                    order.tableId,
-                    new Date(),
-                    order.userId
-                ));
-            });
-            console.log(this.orders);
+                console.log(this._completedOrders);
+            } else {
+                this.errorService.add(result.message);
+            }
         });
     }
     private pay() {
-        let id: string = this.orders[this.selected].id;
+        let id: string = this._completedOrders[this._selectedOrder].id;
         this.orderService.update(id, {
             status: 5 // Archived
         }).subscribe((result) => {
             if(result.status == 1) {
-                this.selected = undefined;
-                this.fetchOrders();
+                this._selectedOrder = undefined;
+                this.fetchCompletedOrders();
+            } else {
+                this.errorService.add(result.message);
             }
         });
     }
